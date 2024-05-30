5eaa555f1c9b00f917f56f57533e97e336e7c3b3
MohamedAdel0301 <mosoliman33@gmail.com>
2024-05-30 04:14:39 +0300

    added more behavior to popover element

--- react-next/rmtDev/src/components/BookmarksButton.tsx
+++ react-next/rmtDev/src/components/BookmarksButton.tsx
@@ -1,19 +1,42 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import BookmarksPopover from "./BookmarksPopover";
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const popoverRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        e.target instanceof HTMLElement &&
+        !buttonRef.current?.contains(e.target) &&
+        !popoverRef.current?.contains(e.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  console.log(isOpen);
+
   return (
     <section>
       <button
+        ref={buttonRef}
         onClick={() => setIsOpen((prev) => !prev)}
         className="bookmarks-btn"
       >
         Bookmarks <TriangleDownIcon />
       </button>
-      {isOpen && <BookmarksPopover />}
+      {isOpen && <BookmarksPopover ref={popoverRef} />}
     </section>
   );
 }
