c30fadf80f069037195b3a7cc7d388daea9fe89e
MohamedAdel0301 <mosoliman33@gmail.com>
2024-05-31 03:49:32 +0300

    Styled navbar

--- react-next/EVENTO/evento/src/components/Header/Header.tsx
+++ react-next/EVENTO/evento/src/components/Header/Header.tsx
@@ -1,18 +1,36 @@
 import Link from "next/link";
 import Logo from "./Logo";
 
+type LinksType = {
+  name: string;
+  path: string;
+};
+
 const Header = () => {
+  const links: LinksType[] = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "All Events",
+      path: "/events/all",
+    },
+  ];
+
   return (
-    <header className="flex h-14 items-center justify-between border-b border-white/10">
+    <header className="flex h-14 items-center justify-between border-b border-white/10 px-3 sm:px-9">
       <Logo />
       <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/events">All Events</Link>
-          </li>
+        <ul className="flex gap-x-6 text-sm">
+          {links.map((link) => (
+            <li
+              key={link.name}
+              className="text-white/50 transition-colors hover:text-white"
+            >
+              <Link href={link.path}>{link.name}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
