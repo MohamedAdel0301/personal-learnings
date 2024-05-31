e892873ef1977690d851b76b78fe2d0c929b0932
MohamedAdel0301 <mosoliman33@gmail.com>
2024-05-31 05:05:37 +0300

    added framer motion to home page

--- react-next/EVENTO/evento/src/app/events/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Events = () => {
-  return <main>Events Pages</main>;
-};
-
-export default Events;
--- react-next/EVENTO/evento/src/components/Header/Header.tsx
+++ react-next/EVENTO/evento/src/components/Header/Header.tsx
@@ -1,6 +1,10 @@
+"use client";
 import Link from "next/link";
 import Logo from "./Logo";
 import { LinkType } from "@/lib/types";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const links: LinkType[] = [
   {
@@ -14,17 +18,30 @@ const links: LinkType[] = [
 ];
 
 const Header = () => {
+  const activePathname = usePathname();
   return (
     <header className="flex h-14 items-center justify-between border-b border-white/10 px-3 md:px-9">
       <Logo />
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className="h-full">
+        <ul className="flex h-full gap-x-6 text-sm">
           {links.map((link) => (
             <li
               key={link.name}
-              className="text-white/50 transition-colors hover:text-white"
+              className={clsx(
+                "relative flex items-center transition-colors hover:text-white",
+                {
+                  "text-white": link.path === activePathname,
+                  "text-white/50": link.path !== activePathname,
+                },
+              )}
             >
               <Link href={link.path}>{link.name}</Link>
+              {activePathname === link.path && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="absolute bottom-0 h-1 w-full bg-accent"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
