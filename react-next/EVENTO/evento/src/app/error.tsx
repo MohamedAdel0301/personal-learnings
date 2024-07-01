207630d482acbd4c6cbed6431d13fb32bd8f04b8
MohamedAdel0301 <mosoliman33@gmail.com>
2024-07-02 00:11:43 +0300

    added error page

---
+++ react-next/EVENTO/evento/src/app/error.tsx
@@ -0,0 +1,26 @@
+"use client"; // Error components must be Client Components
+
+import H1 from "@/components/H1";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <main className="py-24 text-center">
+      <H1 className="mb-5">Something went wrong!</H1>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          reset
+        }
+      >
+        Try again
+      </button>
+    </main>
+  );
+}
