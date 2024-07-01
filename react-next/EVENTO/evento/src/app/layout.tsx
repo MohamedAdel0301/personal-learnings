09543ab1f2f2e0ea4ae1fd080a72e86b26805e76
MohamedAdel0301 <mosoliman33@gmail.com>
2024-07-02 00:45:03 +0300

    sorted more ts errors

--- react-next/EVENTO/evento/src/app/layout.tsx
+++ react-next/EVENTO/evento/src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-import { Inter, Roboto } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "components/header/Header";
 import Footer from "components/Footer";
 import Container from "components/Container";
 
-const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto({
   weight: ["400", "500", "700"],
   fallback: ["sans-serif"],
